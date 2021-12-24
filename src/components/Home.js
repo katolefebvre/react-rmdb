a16d3950eadc352878b561b3e5ee260d9f6c53ea
Kato Lefebvre <klefebvre90@gmail.com>
2021-12-24 01:42:03 -0500

    Session 3: Hero Image and Grid start

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+
+// Components
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+
+// Hooks
+import { useHomeFetch } from "../hooks/useHomeFetch";
+
+// Images
+import NoImage from "../images/no_image.jpg";
+
+const Home = () => {
+  const { state, loading, error } = useHomeFetch();
+
+  return (
+    <>
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      )}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default Home;
