8d2acb788a1ffd695afa8614754a611ca59464f6
Kato Lefebvre <klefebvre90@gmail.com>
2021-12-31 00:29:46 -0500

    Movie page, session storage, prop types

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 
 import API from "../API";
 
+// Helpers
+import { isPersistedState } from "../helpers";
+
 const initialState = {
   page: 0,
   results: [],
@@ -37,18 +40,30 @@ export const useHomeFetch = () => {
 
   // Initial render and search
   useEffect(() => {
+    if (!searchTerm) {
+      const sessionState = isPersistedState("homeState");
+
+      if (sessionState) {
+        setState(sessionState);
+        return;
+      }
+    }
+
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
 
+  // Load more
   useEffect(() => {
-    if (!isLoadingMore) {
-      return;
-    }
-
+    if (!isLoadingMore) return;
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
   }, [isLoadingMore, searchTerm, state.page]);
+
+  // Write to session storage
+  useEffect(() => {
+    if (!searchTerm) sessionStorage.setItem("homeState", JSON.stringify(state));
+  }, [searchTerm, state]);
 
   return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
